4e91d36f11ed6744c0ab8febfda539ab0dd56db6
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2022-07-24 21:49:46 +0200

    Modifying main file for checking package and modules dependence

--- weather.js
+++ weather.js
@@ -1,1 +1,22 @@
 #!/usr/bin/env node //for cli
+
+import {getArgs} from "./helpers/args.js"
+import {printHelp,printSuccess,printError} from "./services/log_services.js";
+
+const initCLI = ()=>{
+    const args = getArgs(process.argv);
+    console.log(args);
+    if(args.h){
+        //Help output
+        printHelp();
+    }
+    if(args.s){
+        //Save City
+    }
+    if(args.t){
+        //Save token
+    }
+    //Output Weather
+};
+
+initCLI();
