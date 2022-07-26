098f1768975622b8b0e5f0fa03806011ecfb9a9d
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2022-07-26 13:27:38 +0200

    Add storage city and tokens in the json file

--- services/storage_services.js
+++ services/storage_services.js
@@ -1,6 +1,49 @@
+import {homedir} from "os";
+import {join} from "path";
+import {promises} from "fs";
 
+const filePath = join(homedir(),"weather-data.json");
 
-const saveKeyValue = (key,value)=>{
+const TOKEN_DICTIONARY = {
+    token: "token",
+    city: "city"
+}
 
+const saveKeyValue = async (key,value)=>{
+    // console.log(basename(filePath));
+    // console.log(dirname(filePath));
+    // console.log(extname(filePath));
+    // console.log(relative(filePath,dirname(filePath)));
+    // console.log(isAbsolute(filePath));
+    // console.log(resolve(".."))
+    // console.log(sep);
+    let data = {};
+    if(await isExist(filePath)){
+        const file = await promises.readFile(filePath);
+        data = JSON.parse(file);
+    }
+    data[key] = value;
+    await promises.writeFile(filePath, JSON.stringify(data));
 };
-export {saveKeyValue};
+
+const getKeyValue = async (key)=>{
+    if(await isExist(filePath)){
+        const file = await promises.readFile(filePath);
+        const data = JSON.parse(file);
+        return data[key];
+    }
+    else{
+        return undefined;
+    }
+};
+
+const isExist = async (path)=>{
+    try {
+        await promises.stat(path);
+        return true;
+    }
+    catch (e){
+        return false;
+    }
+};
+export {saveKeyValue, getKeyValue, TOKEN_DICTIONARY};
